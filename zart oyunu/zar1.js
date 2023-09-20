729db9f970dba0b982402f8236d0f1fe10a6ad8a
musabdmr <musabdemiir@gmail.com>
2023-09-20 20:58:06 +0300

    zar atma

---
+++ zart oyunu/zar1.js
@@ -0,0 +1,8 @@
+
+function zar_at(){
+var zar1,zar2;
+zar1=Math.floor(Math.random()*6+1);
+zar2=Math.floor(Math.random()*6+1);
+document.getElementById("zar1konum").innerHTML= "<img src="+ zar1 +".jpg>";
+document.getElementById("zar2konum").innerHTML="<img src="+ zar2 +".jpg>";
+}
